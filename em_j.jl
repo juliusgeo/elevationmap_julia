7d23c09cd54dfb1e22a6d1c4c938cc0163bbc145
Julius Park <juliusgeo@gmail.com>
2018-09-17 13:03:49 -0400

    got elevation retrieval working

--- em_j.jl
+++ em_j.jl
@@ -1,10 +1,22 @@
 import ArchGDAL
-ArchGDAL.registerdrivers() do
-    ArchGDAL.read("srtm_21_05.tif") do dataset
-        print(dataset)
-        band = ArchGDAL.getband(dataset, 1)
-        geotransform = ArchGDAL.getgeotransform(dataset)
-        print(geotransform)
-        arr = ArchGDAL.read(band)
-    end
+
+function geo2pixel(gt, x, y)
+	pixelY = Int64(round((y-gt[1])/gt[2]))
+	pixelX = Int64(round((x-gt[4])/gt[6]))
+	return (pixelX, pixelY)
 end
+function getelev(x, y)
+	ArchGDAL.registerdrivers() do
+	    ArchGDAL.read("srtm_21_05.tif") do dataset
+	        print(dataset)
+	        band = ArchGDAL.getband(dataset, 1)
+	        geotransform = ArchGDAL.getgeotransform(dataset)
+	        print(geotransform)
+	        arr = ArchGDAL.read(band)
+	        newcoords = geo2pixel(geotransform, x, y)
+	        print(newcoords)
+	        print(arr[newcoords[2], newcoords[1]])
+	    end
+	end
+end
+getelev(38.735375,-77.410316)
