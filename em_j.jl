82391be6398fb3a74ed339347e9128f16afd8233
Julius Park <juliusgeo@gmail.com>
2018-09-17 12:14:48 -0400

    got geotransform

--- em_j.jl
+++ em_j.jl
@@ -1,6 +1,10 @@
-using ArchGDAL
+import ArchGDAL
 ArchGDAL.registerdrivers() do
     ArchGDAL.read("srtm_21_05.tif") do dataset
         print(dataset)
+        band = ArchGDAL.getband(dataset, 1)
+        geotransform = ArchGDAL.getgeotransform(dataset)
+        print(geotransform)
+        arr = ArchGDAL.read(band)
     end
 end
